f448f2cf4ebb9f4aa0d37fd5830a799598020c72
Keshav Sharma <sharmakeshav802@gmail.com>
2026-05-29 10:46:19 +0530

    ToDo App

--- Project_2_ToDoApp/ToDoApp/src/app/app.ts
+++ Project_2_ToDoApp/ToDoApp/src/app/app.ts
@@ -8,5 +8,30 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('ToDoApp');
+  newTodo=signal('');
+
+  todos=signal<{text:string;done:boolean}[]>([]);
+
+  addTodo(){
+    const text=this.newTodo().trim();
+
+    if(text==='') return;
+
+    this.todos.update(list=>[...list,{text,done:false}]);
+
+    this.newTodo.set('');
+  }
+
+  toggleTodo(index:number){
+    this.todos.update(list=>list.map((item,i)=>
+      i===index?{...item,done:!item.done}:item
+    ))
+  }
+
+  DeleteTodo(index:number){
+    this.todos.update(list=>
+      list.filter((_,i)=>i!=index)
+
+    )
+  }
 }
